edb9b4c6e3e15973a51e711ef28cb897a7920714
Ravindra Devrani <ravindra.devrani003@gmail.com>
2025-03-11 21:40:06 +0530

    few more updates in task

--- TrackerNTaskMgr.Api/Entities/SubTask.cs
+++ TrackerNTaskMgr.Api/Entities/SubTask.cs
@@ -5,4 +5,11 @@ public class SubTask : EntityBase
     public int SubTaskId { get; private set; }
     public int TaskId { get; private set; }
     public string Title { get; private set; } = string.Empty;
+    public string? ShortDescription { get; private set; }
+    public Uri? Uri { get; private set; }
+
+    private SubTask(int taskId, string title, string? shortDescription, Uri? uri)
+    {
+
+    }
 }
--- TrackerNTaskMgr.Api/Entities/Task.cs
+++ TrackerNTaskMgr.Api/Entities/Task.cs
@@ -9,12 +9,14 @@ public class Task : EntityBase
     public string Title { get; private set; } = string.Empty;
     public string? ShortDescription { get; private set; }
     public Priority Priority { get; private set; }
-    public Uri Uri { get; private set; } = null!;
+    public Uri? Uri { get; private set; }
     public DateTime? Deadline { get; private set; }
     public DateTime? ScheduledAt { get; private set; }
     public Constants.TaskStatus TaskStatus { get; private set; } = Constants.TaskStatus.Pending;
+    public bool DisplayAtBoard { get; private set; }
 
-    private Task(int taskHeaderId, string title, string? shortDescription, Priority priority, Uri uri, DateTime? deadline, DateTime? scheduledAt)
+
+    private Task(int taskHeaderId, string title, string? shortDescription, Priority priority, Uri uri, DateTime? deadline, DateTime? scheduledAt, bool displayAtBoard)
     {
         TaskHeaderId = taskHeaderId;
         Title = title;
@@ -23,17 +25,18 @@ public class Task : EntityBase
         Uri = uri;
         Deadline = deadline;
         ScheduledAt = scheduledAt;
+        DisplayAtBoard = displayAtBoard;
     }
 
-    public Task Create(int taskHeaderId, string title, string? shortDescription, Priority priority, Uri uri, DateTime? deadline, DateTime? scheduledAt, Constants.TaskStatus taskStatus)
+    public static Task Create(int taskHeaderId, string title, string? shortDescription, Priority priority, Uri uri, DateTime? deadline, DateTime? scheduledAt, Constants.TaskStatus taskStatus, bool displayAtBoard)
     {
-        ValidateInputs(taskHeaderId, title, uri);
-        return new Task(taskHeaderId, title, shortDescription, priority, uri, deadline, scheduledAt);
+        ValidateInputs(taskHeaderId, title, shortDescription);
+        return new Task(taskHeaderId, title, shortDescription, priority, uri, deadline, scheduledAt, displayAtBoard);
     }
 
-    public void Update(int taskHeaderId, string title, string? shortDescription, Priority priority, Uri uri, DateTime? deadline, DateTime? scheduledAt, Constants.TaskStatus taskStatus)
+    public void Update(int taskHeaderId, string title, string? shortDescription, Priority priority, Uri uri, DateTime? deadline, DateTime? scheduledAt, Constants.TaskStatus taskStatus, bool displayAtBoard)
     {
-        ValidateInputs(taskHeaderId, title, uri);
+        ValidateInputs(taskHeaderId, title, shortDescription);
         TaskHeaderId = taskHeaderId;
         Title = title;
         ShortDescription = shortDescription;
@@ -42,10 +45,12 @@ public class Task : EntityBase
         Deadline = deadline;
         ScheduledAt = scheduledAt;
         TaskStatus = taskStatus;
+        DisplayAtBoard = displayAtBoard;
+
         ModifyUpdated();
     }
 
-    private void ValidateInputs(int taskHeaderId, string title, Uri uri)
+    private static void ValidateInputs(int taskHeaderId, string title, string? shortDescription)
     {
         if (taskHeaderId < 0)
         {
@@ -57,10 +62,12 @@ public class Task : EntityBase
             throw new ArgumentException("Title can't be null", nameof(taskHeaderId));
         }
 
-        if (uri == null)
+        if (!string.IsNullOrWhiteSpace(shortDescription))
         {
-            throw new ArgumentException("Uri can't be null", nameof(taskHeaderId));
+            if (shortDescription.Length > 200)
+            {
+                throw new ArgumentException("", nameof(shortDescription));
+            }
         }
-
     }
 }
