c24ceec1e2367fb0c9732db099730bdeb2a9ca37
Ravindra Devrani <ravindra.devrani003@gmail.com>
2025-03-15 13:55:46 +0530

    few methods has created in TrackEntryService

--- TrackerNTaskMgr.Api/Mappings/TrackEntryMapper.cs
+++ TrackerNTaskMgr.Api/Mappings/TrackEntryMapper.cs
@@ -1,21 +1,19 @@
 using TrackerNTaskMgr.Api.DTOs;
+using TrackerNTaskMgr.Api.Entities;
 
 namespace TrackerNTaskMgr.Api.Mappings;
 
 public static class TrackEntryMapper
 {
-    public static TrackEntryReadDto ToTrackEntryReadDto(this TrackEntryCreateDto dto)
-    {
-        return new TrackEntryReadDto
-        (
-            0,
-            dto.EntryDate,
-            dto.SleptAt,
-            dto.WokeUpAt,
-            dto.NapInMinutes,
-            0,
-            dto.TotalWorkInMinutes,
-            null
-         );
-    }
+    // public static TrackEntry ToTrackEntry(this TrackEntryCreateDto dto)
+    // {
+    //     return new TrackEntry
+    //     {
+    //         EntryDate = dto.EntryDate,
+    //         SleptAt = dto.SleptAt,
+    //         WokeUpAt = dto.WokeUpAt,
+    //         NapInMinutes = dto.NapInMinutes,
+    //         TotalWorkInMinutes = dto.TotalWorkInMinutes,
+    //     };
+    // }
 }
--- TrackerNTaskMgr.Api/Services/TrackEntryService.cs
+++ TrackerNTaskMgr.Api/Services/TrackEntryService.cs
@@ -1,7 +1,10 @@
 using System.Data;
+using System.Transactions;
 using Dapper;
 using Microsoft.Data.SqlClient;
 using TrackerNTaskMgr.Api.DTOs;
+using TrackerNTaskMgr.Api.Entities;
+using TrackerNTaskMgr.Api.Mappings;
 
 namespace TrackerNTaskMgr.Api.Services;
 
@@ -15,11 +18,88 @@ public class TrackEntryService
         _config = config;
     }
 
-    public async Task<TrackEntryReadDto> CreateTrackEntryAsync(TrackEntryCreateDto trackEntry)
+    public async Task<TrackEntryReadDto?> CreateTrackEntryAsync(TrackEntryCreateDto trackEntryToCreate)
     {
         using IDbConnection connection = new SqlConnection(_connectionString);
-        string query = "";
-        int id = await connection.ExecuteScalarAsync<int>(query, trackEntry);
-        return new TrackHead
+        // Craete task entry
+        // create task remark if remark in not null
+        // return created track entry with related data
+        // use transaction
+        using var scope = new TransactionScope();
+        string trackEntryQuery = @"insert into TrackEntries(EntryDate,SleptAt,WokeUpAt,NapInMinutes,TotalWorkInMinutes)
+                         values (@EntryDate,@SleptAt,@WokeUpAt,@NapInMinutes,@TotalWorkInMinutes);
+                         select scope_identity();
+                         ";
+        int trackEntryId = await connection.ExecuteScalarAsync<int>(trackEntryQuery, trackEntryToCreate);
+
+        if (!string.IsNullOrWhiteSpace(trackEntryToCreate.Remarks))
+        {
+            string trackEntryRemarkQuery = @"insert into TrackEntryRemarks (TrackEntryId,Remarks)
+                                             values(@TrackEntryId,@Remarks);";
+            await connection.ExecuteAsync(trackEntryRemarkQuery, new { TrackEntryId = trackEntryId, trackEntryToCreate.Remarks });
+        }
+        scope.Complete();
+        return await GetTrackEntryAsync(trackEntryId);
+    }
+
+    public async Task<TrackEntryReadDto?> GetTrackEntryAsync(int id)
+    {
+        using IDbConnection connection = new SqlConnection(_connectionString);
+        string sql = @"select  
+                         te.TrackEntryId,
+                         te.EntryDate,
+                         te.SleptAt,
+                         te.WokeUpAt,
+                         te.NapInMinutes,
+                         te.TotalSleep,
+                         te.TotalWorkInMinutes
+                         tr.TrackEntryId,
+                         tr.Remarks
+                       from TrackEntries te
+                       left join TrackEntryRemarks tr
+                       on te.TrackEntryId = tr.TrackEntryId
+                       where te.Deleted is null te.TrackEntryId=@id
+                       order by te.EntryDate desc
+                        ";
+        TrackEntryReadDto? trackEntry = (await connection.QueryAsync<TrackEntryReadDto, TrackEntryRemarkReadDto, TrackEntryReadDto>(
+             sql: sql,
+             map: (entry, remark) =>
+             {
+                 return entry with { TrackEntryRemark = remark }; // TrackEntryRemark is a Record we can't mutate it directly
+             },
+             param: new { id },
+             splitOn: "TrackEntryId"
+             )).FirstOrDefault();
+        return trackEntry;
+    }
+
+    public async Task<IEnumerable<TrackEntryReadDto>> GetTrackEntiesAsync()
+    {
+        using IDbConnection connection = new SqlConnection(_connectionString);
+        string sql = @"select  
+                         te.TrackEntryId,
+                         te.EntryDate,
+                         te.SleptAt,
+                         te.WokeUpAt,
+                         te.NapInMinutes,
+                         te.TotalSleep,
+                         te.TotalWorkInMinutes
+                         tr.TrackEntryId,
+                         tr.Remarks
+                       from TrackEntries te
+                       left join TrackEntryRemarks tr
+                       on te.TrackEntryId = tr.TrackEntryId
+                       where te.Deleted is null
+                       order by te.EntryDate desc
+                        ";
+        IEnumerable<TrackEntryReadDto> trackEntries = await connection.QueryAsync<TrackEntryReadDto, TrackEntryRemarkReadDto, TrackEntryReadDto>(
+             sql: sql,
+             map: (entry, remark) =>
+             {
+                 return entry with { TrackEntryRemark = remark }; // TrackEntryRemark is a Record we can't mutate it directly
+             },
+             splitOn: "TrackEntryId"
+             );
+        return trackEntries;
     }
 }
