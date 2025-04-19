e32e1adce21a0df37db2771813433a7094e9acb2
Ravindra Devrani <ravindra.devrani003@gmail.com>
2025-04-19 13:51:51 +0530

    feat: routing applied

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,5 +1,17 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
 
 export const routes: Routes = [{
-    
-}];
+   path:"dashboard",
+   component: DashboardComponent       
+},
+{
+    path:"",
+    redirectTo:"/dashboard",
+    pathMatch:"full"
+},
+{
+    path:"**",
+    loadComponent:()=>import("./not-found.component").then(a=>a.NotFoundComponent),
+}
+];
