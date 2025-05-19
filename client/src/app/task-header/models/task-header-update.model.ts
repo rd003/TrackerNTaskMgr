fcdf76665ccbe8d084bffd7f851211992222c5bf
Ravindra Devrani <ravindra.devrani003@gmail.com>
2025-05-19 17:13:32 +0530

    feat: task header service created

--- client/src/app/task-header/models/task-header-update.model.ts
+++ client/src/app/task-header/models/task-header-update.model.ts
@@ -1,5 +1,5 @@
 import { TaskHeaderCreateModel } from "./task-header-create.model";
 
-export interface TaskHeaderUpdatModel extends TaskHeaderCreateModel {
+export interface TaskHeaderUpdateModel extends TaskHeaderCreateModel {
     taskHeaderId: number
 }
--- client/src/app/task-header/services/task-header.service.ts
+++ client/src/app/task-header/services/task-header.service.ts
@@ -2,11 +2,30 @@ import { inject, Injectable } from "@angular/core";
 import { environment } from "../../../environments/environment.development";
 import { HttpClient } from "@angular/common/http";
 import { TaskHeaderReadModel } from "../models/task-header-read.model";
+import { TaskHeaderCreateModel } from "../models/task-header-create.model";
+import { TaskHeaderUpdateModel } from "../models/task-header-update.model";
+
 
 @Injectable({ providedIn: 'root' })
 export class TaskHeaderService {
     private readonly url = environment.baseUrl + "/task-headers";
     private readonly http = inject(HttpClient);
+
+    addTaskHeader(taskHeader: TaskHeaderCreateModel) {
+        return this.http.post<TaskHeaderReadModel>(this.url, taskHeader);
+    }
+
+    updateTaskHeader(taskHeader: TaskHeaderUpdateModel) {
+        return this.http.put<TaskHeaderUpdateModel>(`${this.url}/${taskHeader.taskHeaderId}`, taskHeader);
+    }
+
+    getTaskHeader(id: number) {
+        return this.http.get<TaskHeaderUpdateModel>(`${this.url}/${id}`);
+    }
+
+    deleteTaskHeader(id: number) {
+        return this.http.delete<TaskHeaderUpdateModel>(`${this.url}/${id}`);
+    }
 
     getTaskHeaders() {
         return this.http.get<TaskHeaderReadModel[]>(this.url);
